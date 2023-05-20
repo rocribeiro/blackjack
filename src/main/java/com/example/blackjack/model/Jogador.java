047649c85834a39c619e776c3d02843050d4059e
Rodrigo Ribeiro <rodrigo.ribeiro03@hotmail.com>
2023-05-20 18:53:34 -0300

    Implementado model Jogador

---
+++ src/main/java/com/example/blackjack/model/Jogador.java
@@ -0,0 +1,56 @@
+package com.example.blackjack.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jogador {
+    private String nome;
+    private List<Carta> jogoEmMaos;
+
+    private int pontuacao;
+
+    public Jogador(){
+
+    }
+    public Jogador(String nome) {
+        this.nome = nome;
+        this.jogoEmMaos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Carta> getJogoEmMaos() {
+        return jogoEmMaos;
+    }
+
+    public void setJogoEmMaos(List<Carta> jogoEmMaos) {
+        this.jogoEmMaos = jogoEmMaos;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public void receberUmaCarta(Carta carta) {
+        jogoEmMaos.add(carta);
+        setPontuacao(getPontuacao()+carta.getValor());
+    }
+
+    @Override
+    public String toString() {
+        return "Jogador{" +
+                "nome='" + nome + '\'' +
+                ", jogoEmMaos=" + jogoEmMaos.toString() +
+                '}';
+    }
+}
