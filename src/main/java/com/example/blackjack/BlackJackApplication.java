b61f76458e37b00fd221a0d742afbf7fc7c4e83c
Rodrigo Ribeiro <rodrigo.ribeiro03@hotmail.com>
2023-05-22 16:47:16 -0300

    adicionando instancia do jogo

--- src/main/java/com/example/blackjack/BlackJackApplication.java
+++ src/main/java/com/example/blackjack/BlackJackApplication.java
@@ -1,13 +1,13 @@
 package com.example.blackjack;
 
+import com.example.blackjack.model.BlackjackGame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlackJackApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(BlackJackApplication.class, args);
+        BlackjackGame jogo = new BlackjackGame();
+        jogo.iniciarPartida();
     }
-
 }
